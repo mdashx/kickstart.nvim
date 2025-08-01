5433f9ca42748dd7aeb0991c698da4e76f8da3aa
tom <tom@mdashx.com>
2025-08-01 14:23:25 -0600

    My snippets

--- init.lua
+++ init.lua
@@ -849,7 +849,9 @@ require('lazy').setup({
             'rafamadriz/friendly-snippets',
             config = function()
               require('luasnip.loaders.from_vscode').lazy_load()
-              -- require('luasnip.loaders.from_vscode').lazy_load { paths = { './my_snippets' } }
+              require('luasnip.loaders.from_vscode').lazy_load {
+                paths = { vim.fn.stdpath 'config' .. '/my_snippets' },
+              }
             end,
           },
         },
